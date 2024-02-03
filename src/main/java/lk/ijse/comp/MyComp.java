4f276ad666c86fa16fca80ca0108f4ebe621f118
NimnaKs <nimnakaveesha123456@gmail.com>
2024-02-03 13:39:47 +0530

    Comp done

---
+++ src/main/java/lk/ijse/comp/MyComp.java
@@ -0,0 +1,10 @@
+package lk.ijse.comp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyComp {
+    MyComp(){
+        System.out.println("MyComp");
+    }
+}
--- src/main/java/lk/ijse/configs/AppConfig.java
+++ src/main/java/lk/ijse/configs/AppConfig.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.ComponentScans;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "lk.ijse.components")
+@ComponentScan(basePackages = "lk.ijse")
 public class AppConfig {
 }
