37dfd51c553df59108d9efd6024984d8d7d39561
NimnaKs <nimnakaveesha123456@gmail.com>
2024-02-03 13:15:05 +0530

    AppConfig and Mycomponent  done

--- src/main/java/lk/ijse/components/MyComponent.java
+++ src/main/java/lk/ijse/components/MyComponent.java
@@ -1,4 +1,9 @@
 package lk.ijse.components;
 
+import org.springframework.stereotype.Component;
+@Component
 public class MyComponent {
+    MyComponent(){
+        System.out.println("My Component");
+    }
 }
--- src/main/java/lk/ijse/configs/AppConfig.java
+++ src/main/java/lk/ijse/configs/AppConfig.java
@@ -1,4 +1,10 @@
 package lk.ijse.configs;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.components")
 public class AppConfig {
 }
