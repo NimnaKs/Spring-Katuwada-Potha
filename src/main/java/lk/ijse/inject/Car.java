edcd6cc0c48da162e300013516559bbb820ddd3e
NimnaKs <nimnakaveesha123456@gmail.com>
2024-02-17 13:09:24 +0530

    Expand done

--- src/main/java/lk/ijse/inject/Car.java
+++ src/main/java/lk/ijse/inject/Car.java
@@ -7,12 +7,15 @@ import org.springframework.stereotype.Component;
 public class Car {
     private final Engine engine;
 
+    private final Engine2 engine2;
+
     @Autowired
-    public Car(Engine engine) {
+    public Car(Engine engine, Engine2 engine2) {
         this.engine = engine;
+        this.engine2 = engine2;
     }
 
     public String start(){
-       return this.engine.V12();
+       return this.engine.V12()+this.engine2.V13();
     }
 }
---
+++ src/main/java/lk/ijse/inject/Engine2.java
@@ -0,0 +1,11 @@
+package lk.ijse.inject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Engine2 {
+    public String V13() {
+        return "V13 Power";
+    }
+
+}
